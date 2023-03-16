088084f3434bdd0eae0d83b14b8c68ece7fe2184
AlexandreDinizVeloso <alexandredinizveloso@gmail.com>
2023-03-16 16:41:37 -0300

    Modificação para pesquisa de Pokemon

--- scripts.js
+++ scripts.js
@@ -1,23 +1,23 @@
-let url = "https://pokemon.danielpimentel.com.br/v1/pokemon/geracao/1/50/1";
-
-fetch(url)
-        .then((resposta) => resposta.json())
-        .then((pokemon) => carregaPokedex(pokemon.pokemon))
-        .catch((erro) => console.log(erro))
+function acessarPokedex() {
+    let pokemon = document.getElementById("nomePokemon").value
+    let url = `https://pokemon.danielpimentel.com.br/v1/pokemon/nome/${pokemon}`
+    fetch(url)
+            .then((resposta) => resposta.json())
+            .then((pokemon) => carregaPokedex(pokemon.pokemon))
+            .catch((erro) => console.log(erro))
+}
 
 function carregaPokedex(pokemon) {
-    let pokedex = document.getElementById("pokedex");
-    pokedex.innerHTML = ""
-    for (let i = 0; i < pokemon.length; i++) {
-        let html = `
-            <div class="molduraPokemon">
-                <img src="${pokemon[i].img_3d}" heigth="150" /> <br>
-                <div class="infoPokemon">
-                    ${pokemon[i].nome} - ${pokemon[i].numero}
+    let pesquisaPokemon = document.getElementById("pesquisaPokemon");
+    pesquisaPokemon.innerHTML = ""
+    let html = `
+        <div class="molduraPokemon">
+            <img src="${pokemon.img_3d}" heigth="150" /> <br>
+            <div class="infoPokemon">
+                ${pokemon.nome} - ${pokemon.numero}
 
-                </div>
             </div>
-        `;    
-        pokedex.insertAdjacentHTML("beforeend", html);
-    }
+        </div>
+    `;
+    pesquisaPokemon.insertAdjacentHTML("beforeend", html);
 }
